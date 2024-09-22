abda4cb997dec4bf2cadb57e6159e3bb4645b7ab
Bui Duc Hung <duchungbui1@gmail.com>
2024-09-22 11:02:37 +0700

    1. Validate du lieu

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -16,6 +16,8 @@ const upload = multer({ storage: storage });
 
 const controller = require("../../controllers/admin/products.controller");
 
+const validate = require("../../validates/admin/product.validate");
+
 router.get("/", controller.index);
 
 router.patch("/change-status", controller.changeStatus);
@@ -31,6 +33,7 @@ router.get("/create", controller.create);
 router.post(
   "/create", 
   upload.single('thumbnail'),
+  validate.createPost,
   controller.createPost 
 );
 
