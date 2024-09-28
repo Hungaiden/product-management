edf19c9007eef8c25800c86504b4109a2bdecbca
Bui Duc Hung <duchungbui1@gmail.com>
2024-09-28 16:23:58 +0700

    3.4 Xay dung phan chinh sua nhom quyen

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 
 module.exports.index = async (req, res) => {
@@ -15,4 +16,38 @@ module.exports.create = async(req, res) => {
     res.render("admin/pages/role/create", {
         pageTitle: "Trang thêm mới",       
     });
+}
+
+module.exports.createPost = async(req, res) => {
+    const role = new Role(req.body);
+    await role.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+}
+
+module.exports.edit = async(req, res) => {
+    const id = req.params.id;
+
+    const role = await Role.findOne({
+        deleted: false,
+        _id: id
+    });
+    
+    res.render("admin/pages/role/edit", {
+        pageTitle: "Trang chỉnh sửa",     
+        role: role  
+    });
+}
+
+module.exports.editPatch = async(req, res) => {
+    const id = req.params.id;
+
+    await Role.updateOne({
+        _id: id,
+        deleted: false
+      }, req.body);
+
+      console.log(req.body);
+      req.flash("success", "Cập nhật thành công!");
+      res.redirect(`back`);
 }
