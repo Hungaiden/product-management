814506b23c68de24ff3c2fb5d3af18099aa12f8e
Bui Duc Hung <duchungbui1@gmail.com>
2024-09-28 08:28:21 +0700

    3. Tao danh muc san pham + Hien thi danh sach danh muc

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,57 @@
+const ProductCategory = require("../../models/product-category.model");
+const systemConfig = require("../../config/system");
+
+module.exports.index = async(req, res) => {
+  const listCategory = await ProductCategory.find({
+    deleted: false
+  });
+
+
+  res.render("admin/pages/products-category/index", {
+    listCategory: listCategory,
+    pageTitle: "Danh sách danh mục sản phẩm"
+  });
+}
+
+
+module.exports.create = async (req, res) => {
+  const listCategory = await ProductCategory.find({
+    deleted: false
+  });
+  res.render("admin/pages/products-category/create", {
+    pageTitle: "Tạo danh mục sản phẩm",
+    listCategory: listCategory
+  });
+}
+module.exports.createPost = async (req, res) => {
+  if(req.body.position) {
+    req.body.position = parseInt(req.body.position);
+  } else {
+    const countRecord = await ProductCategory.countDocuments();
+    req.body.position = countRecord + 1;
+  }
+  const record = new ProductCategory(req.body);
+  await record.save();
+  res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+}
+
+module.exports.edit = async (req, res) => {
+  const id = req.params.id;
+  
+  const category = await ProductCategory.findOne({
+    deleted: false,
+    _id: id
+  });
+
+  const listCategory = await ProductCategory.find({
+    deleted: false
+  });
+
+  res.render(
+    "admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa sản phẩm",
+      listCategory: listCategory,
+      category: category
+    }
+  );
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,8 @@
 const dashboardRoute = require("./dashboard.route");
 
-const productRoute = require("./products.route");
+const productRoute = require("./product.route");
+
+const productCategoryRoute = require("./product-category.route");
 
 const systemConfig = require("../../config/system");
 
@@ -9,9 +11,8 @@ module.exports.index = (app) => {
   const PATH_ADMIN = `/admin`
   app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
 
-
-
-
   app.use(`${PATH_ADMIN}/products`, productRoute);
+
+  app.use(`${PATH_ADMIN}/products-category`, productCategoryRoute);
 
 }
