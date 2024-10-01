89f7fb489fb9a5b3bfe8f42fdb3561f870f5daf2
Bui Duc Hung <duchungbui1@gmail.com>
2024-10-01 21:52:56 +0700

    1.3 Làm trang tao tai khoan + 1.4. Sua trang danh sach tai khoan

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,5 +1,42 @@
-module.exports.index = (req, res) => {
-    res.render("admin/pages/account/index", {
-      pageTitle: "Danh sách tài khoản"
+const Role = require("../../models/role.model");
+const Account = require("../../models/account.model");
+const md5 = require("md5");
+
+const generateHelper = require("../../helpers/generate.helper");
+const systemConfig = require("../../config/system");
+module.exports.index = async(req, res) => {
+  const records = await Account.find({
+    deleted: false
+  });
+  for(const item of records) {
+    const role = await Role.findOne({
+      deleted: false,
+      _id: item.role_id
     });
+
+    item.role_title = role.title;
   }
+  res.render("admin/pages/account/index", {
+    pageTitle: "Danh sách tài khoản",
+    records: records
+  });
+}
+
+module.exports.create = async(req, res) => {
+  const roles = await Role.find({
+    deleted: false,
+  })
+  res.render("admin/pages/account/create", {
+    pageTitle: "Tạo tài khoản",
+    roles: roles
+  });
+}
+
+module.exports.createPost = async(req, res) => {
+  req.body.password = md5(req.body.password);
+  req.body.token = generateHelper.generateRandomString(30);
+  const account = new Account(req.body);
+  await account.save();
+  
+  res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+}
