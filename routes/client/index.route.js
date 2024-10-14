a6e61035670ed4d04c5edd39fa7cf2da46ed1307
Bui Duc Hung <duchungbui1@gmail.com>
2024-10-14 10:13:52 +0700

    2.1 Them san pham vao gio hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,11 +1,18 @@
 const productRoute = require("./products.route"); // nhung file vao
 const homeRouter = require("./home.route");
+const cartRoute = require("./cart.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 module.exports.index = (app) => { // xay dung ham route
     app.use(categoryMiddleware.category);
+    
+    app.use(cartMiddleware.cart);
     
     app.use("/", homeRouter);
       
     app.use("/products/", productRoute);
+
+    app.use("/cart", cartRoute);
 }
