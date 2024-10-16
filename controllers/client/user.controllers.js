d71797077d71d022b550baa3f44a9e574c341f2c
Bui Duc Hung <duchungbui1@gmail.com>
2024-10-16 11:14:13 +0700

    2.3 Đăng nhap

--- controllers/client/user.controllers.js
+++ controllers/client/user.controllers.js
@@ -3,7 +3,9 @@ const md5 = require("md5");
 const generateHelper = require("../../helpers/generate.helper");
 
 module.exports.register = async(req, res) => {
-    res.render("client/pages/user/register");
+    res.render("client/pages/user/register",{
+        pageTitle: "Trang đăng ký"
+    });
 }
 
 module.exports.registerPost = async(req, res) => {
@@ -34,4 +36,41 @@ module.exports.registerPost = async(req, res) => {
     res.cookie("tokenUser", newUser.token);
     req.flash("success", "Đăng ký tài khoản thành công!");
     res.redirect("/");
+}
+
+module.exports.login = async(req, res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Trang đăng nhập"
+    });
+}
+
+module.exports.loginPost = async(req, res) => {
+    const user = req.body;
+    const existUser = await User.findOne({
+        email: user.email,
+        deleted: false
+    });
+
+    if(!existUser) {
+        req.flash("error", "Tài khoản không tồn tại");
+        res.redirect("back");
+        return;
+    }
+
+    if(md5(user.password) != existUser.password) {
+        req.flash("error", "Sai mật khẩu");
+        res.redirect("back");
+        return;
+    }
+
+    if(existUser.status == "inactive") {
+        req.flash("error", "Tài khoản bị khoá");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("tokenUser", existUser.token);
+    req.flash("success", "Đăng nhập thành công!");
+    res.redirect("/");
+    
 }
