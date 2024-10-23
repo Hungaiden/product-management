7e8ca86b65be47cb13592e9b90dd6088662f7935
Bui Duc Hung <duchungbui1@gmail.com>
2024-10-23 22:02:52 +0700

    1.1 Chen icon

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,3 +1,4 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js';
 var socket = io();
 
 // CLIENT_SEND_MESSAGE
@@ -50,3 +51,27 @@ if(bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 // End Scroll Chat To Bottom
+
+
+// Show icon
+const emojiPicker = document.querySelector("emoji-picker");
+
+if(emojiPicker) {
+  const buttonIcon = document.querySelector('.chat .inner-form .button-icon');
+  const tooltip = document.querySelector('.tooltip');
+
+  Popper.createPopper(buttonIcon, tooltip);
+
+  console.log(buttonIcon);
+  buttonIcon.addEventListener("click", () => {
+    tooltip.classList.toggle('shown');
+  });
+
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+  emojiPicker.addEventListener('emoji-click', (event) => {
+    emojiPicker.addEventListener('emoji-click', event => {
+      inputChat.value = inputChat.value + event.detail.unicode;
+    });
+  });
+}
+// end show icon  
